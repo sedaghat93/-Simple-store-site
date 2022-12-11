54fcfd36fff1a80d1863b212800d15432902e34b
sedaqat <rahelehsedaghat93@gmail.com>
2022-12-11 21:23:12 +0330

    Add Project

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// $(function() {
+//     $('[data-toggle="tooltip"]').tooltip();
+
+//     $('.btn-group-fab').on('click', function() {
+//         $('.btn-group-fab').toggleClass('active');
+//     });
+// });
